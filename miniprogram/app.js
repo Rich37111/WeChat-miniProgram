c05f235e5081b224eed1b17d114185e6bd2c1ecd
Rich37111 <1660415124@qq.com>
2021-04-23 14:39:53 +0800

    Initial Commit

---
+++ miniprogram/app.js
@@ -0,0 +1,18 @@
+//相当于入口文件，非常重要
+const config = require("config.js");
+App({
+      openid: '',
+      userinfo:'',
+      canReflect:true,
+      onLaunch: function() {
+            if (!wx.cloud) {
+                  console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+            } else {
+                  wx.cloud.init({
+                       env: JSON.parse(config.data).env,
+                        traceUser: true,
+                  })
+            }
+           this.systeminfo=wx.getSystemInfoSync();
+      }
+})
